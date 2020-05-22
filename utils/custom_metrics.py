73386156d9e0ec0827039ff634c0b30b4f7cfb42
Dat Nguyen <nguyenxuandat1998@gmail.com>
2020-05-22 11:31:33 +0700

    feat: Add custom metric "Recall"

--- utils/custom_metrics.py
+++ utils/custom_metrics.py
@@ -27,3 +27,29 @@ def accuracy(threshold=0.5):
         count = reduce_sum(matches)
         return tf.cond(total == 0, return_0_5, return_acc)
     return acc
+
+def recall(threshold=0.5):
+    """
+    Recall metric for MTCNN. This function will predict whether an image has
+    a face or not using the first output of the model. If the model output a
+    probability >= a threshold then that image is predicted as having a face.
+    After that, it will compare the result with the ground truth to compute 
+    recall. If a batch has no positives and negatives, the recall will be
+    defaulted to 0.5 (approximated random chance).
+
+    Args:
+    - threshold: a float threshold in range [0, 1]. Default is 0.5
+
+    Return:
+    - a function with the signature result = fn(y_true, y_pred)
+    """
+    def rec(y_true, y_pred):
+        def return_0_5():
+            return 0.5
+        def return_rec():
+            return count / total
+        total = reduce_sum(y_true[:, 1])
+        pred = tf.cast(y_pred[:, 1] >= threshold, dtype=tf.uint8) * y_true[:, 0]
+        count = reduce_sum(tf.cast(y_true[:, 1] + pred == 2, dtype=tf.uint8))
+        return tf.cond(total == 0, return_0_5, return_rec)
+    return rec
