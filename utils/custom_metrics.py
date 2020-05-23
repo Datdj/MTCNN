e5f9a43ddd3efec09349e133a20d3b1f94bcb220
Dat Nguyen <nguyenxuandat1998@gmail.com>
2020-05-23 19:54:20 +0700

    fix: Fix the bug with the dtype float32

--- utils/custom_metrics.py
+++ utils/custom_metrics.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.math import reduce_sum
 
-def accuracy(threshold=0.5):
+def accuracy_(threshold=0.5):
     """
     Accuracy metric for MTCNN. This function will predict whether an image has
     a face or not using the first output of the model. If the model output a
@@ -16,19 +16,19 @@ def accuracy(threshold=0.5):
     Return:
     - a function with the signature result = fn(y_true, y_pred)
     """
-    def acc(y_true, y_pred):
+    def accuracy(y_true, y_pred):
         def return_0_5():
             return 0.5
         def return_acc():
             return count / total
         total = reduce_sum(y_true[:, 0])
-        pred = tf.cast(y_pred[:, 1] >= threshold, dtype=tf.uint8) * y_true[:, 0]
-        matches = tf.cast(pred == y_true[:, 1], dtype=tf.uint8) * y_true[:, 0]
+        pred = tf.cast(y_pred[:, 1] >= threshold, dtype=tf.float32) * y_true[:, 0]
+        matches = tf.cast(pred == y_true[:, 1], dtype=tf.float32) * y_true[:, 0]
         count = reduce_sum(matches)
         return tf.cond(total == 0, return_0_5, return_acc)
-    return acc
+    return accuracy
 
-def recall(threshold=0.5):
+def recall_(threshold=0.5):
     """
     Recall metric for MTCNN. This function will predict whether an image has
     a face or not using the first output of the model. If the model output a
@@ -43,13 +43,13 @@ def recall(threshold=0.5):
     Return:
     - a function with the signature result = fn(y_true, y_pred)
     """
-    def rec(y_true, y_pred):
+    def recall(y_true, y_pred):
         def return_0_5():
             return 0.5
         def return_rec():
             return count / total
         total = reduce_sum(y_true[:, 1])
-        pred = tf.cast(y_pred[:, 1] >= threshold, dtype=tf.uint8) * y_true[:, 0]
-        count = reduce_sum(tf.cast(y_true[:, 1] + pred == 2, dtype=tf.uint8))
+        pred = tf.cast(y_pred[:, 1] >= threshold, dtype=tf.float32) * y_true[:, 0]
+        count = reduce_sum(tf.cast(y_true[:, 1] + pred == 2, dtype=tf.float32))
         return tf.cond(total == 0, return_0_5, return_rec)
-    return rec
+    return recall
