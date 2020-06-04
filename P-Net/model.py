46bc44c1fbca8ca1f2bbd032abf59a1d942673e2
Dat Nguyen <nguyenxuandat1998@gmail.com>
2020-06-04 16:45:04 +0700

    feat: Move zero centering to inside the model

--- P-Net/model.py
+++ P-Net/model.py
@@ -3,14 +3,21 @@ from tensorflow import keras
 from tensorflow.keras import Input, Model
 from tensorflow.keras.layers import Layer, Conv2D, PReLU, MaxPool2D, Lambda
 
-def pad_0(num_neurons, batch_size):
+def pad_0(num_neurons):
     def pad(input_tensor):
+        batch_size = tf.shape(input_tensor)[0]
         return tf.concat(values=[tf.zeros(shape=(batch_size, 1)), tf.reshape(tensor=input_tensor, shape=(batch_size, num_neurons))], axis=-1)
     return pad
 
-def pnet(batch_size):
+def zero_center(mean):
+    def zc(input_tensor):
+        return input_tensor - mean
+    return zc
+
+def pnet(mean):
     inputs = Input(shape=(12, 12, 3), name='inputs')
-    conv_1 = Conv2D(filters=10, kernel_size=(3, 3), kernel_initializer='he_normal', name='conv_1')(inputs)
+    zero_centered_inputs = Lambda(function=zero_center(mean), name='zero_center')(inputs)
+    conv_1 = Conv2D(filters=10, kernel_size=(3, 3), kernel_initializer='he_normal', name='conv_1')(zero_centered_inputs)
     prelu_1 = PReLU(name='prelu_1')(conv_1)
     pool_1 = MaxPool2D(name='pool_1')(prelu_1)
     conv_2 = Conv2D(filters=16, kernel_size=(3, 3), kernel_initializer='he_normal', name='conv_2')(pool_1)
@@ -18,10 +25,10 @@ def pnet(batch_size):
     conv_3 = Conv2D(filters=32, kernel_size=(3, 3), kernel_initializer='he_normal', name='conv_3')(prelu_2)
     prelu_3 = PReLU(name='prelu_3')(conv_3)
     output_1 = Conv2D(filters=2, kernel_size=(1, 1), activation='softmax', kernel_initializer='lecun_normal', name='output_1')(prelu_3)
-    padded_output_1 = Lambda(function=pad_0(num_neurons=2, batch_size=batch_size), name='class')(output_1)
+    padded_output_1 = Lambda(function=pad_0(num_neurons=2), name='class')(output_1)
     output_2 = Conv2D(filters=4, kernel_size=(1, 1), kernel_initializer='lecun_normal', name='output_2')(prelu_3)
-    padded_output_2 = Lambda(function=pad_0(num_neurons=4, batch_size=batch_size), name='bbox')(output_2)
+    padded_output_2 = Lambda(function=pad_0(num_neurons=4), name='bbox')(output_2)
     output_3 = Conv2D(filters=10, kernel_size=(1, 1), kernel_initializer='lecun_normal', name='output_3')(prelu_3)
-    padded_output_3 = Lambda(function=pad_0(num_neurons=10, batch_size=batch_size), name='landmark')(output_3)
+    padded_output_3 = Lambda(function=pad_0(num_neurons=10), name='landmark')(output_3)
     model = Model(inputs=inputs, outputs=[padded_output_1, padded_output_2, padded_output_3])
     return model
--- utils/data_augmentation.py
+++ utils/data_augmentation.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
-
-def augment_and_zero_center(mean):
+    
+def augment(images, labels):
     """
     Data augmentation for training MTCNN. This function will take in
     a batch of images and randomly flip, adjust brightness, contrast,
-    hue, saturation of those images. It will also zero center the images.
+    hue, saturation of those images.
     
     Args:
     - images: a 4-D tensor of shape [batch size, height, width, channels]
@@ -17,63 +17,56 @@ def augment_and_zero_center(mean):
     Return:
     a tuple of new (images, labels) that has been augmented
     """
-    
-    def augment(images, labels):
 
-        # Get the images, bounding boxes and landmarks from the input batch
-        bboxes = labels[1]
-        landmarks = labels[2]
-        batch_size = images.shape[0]
+    # Get the images, bounding boxes and landmarks from the input batch
+    bboxes = labels[1]
+    landmarks = labels[2]
+    batch_size = images.shape[0]
 
-        # Randomly flip the images horizontaly
-        random_indices = tf.where(tf.random.uniform([batch_size], 0, 2, tf.int32))
-        # Randomly flip the images
-        images = tf.tensor_scatter_nd_update(images, random_indices, tf.image.flip_left_right(tf.gather_nd(images, random_indices)))
-        # Randomly flip the bounding boxes
-        random_bb = tf.gather_nd(bboxes, random_indices)
-        flipped_bb_x = (1 - random_bb[:, 1] - random_bb[:, 3]) * random_bb[:, 0]
-        flipped_bb = tf.concat([tf.reshape(random_bb[:, 0], [-1, 1]), tf.reshape(flipped_bb_x, [-1, 1]), random_bb[:, 2:]], axis=1)
-        bboxes = tf.tensor_scatter_nd_update(bboxes, random_indices, flipped_bb)
-        # Randomly flip the landmarks
-        random_lm = tf.gather_nd(landmarks, random_indices)
-        flipped_lm_xs = (1 - random_lm[:, 1::2]) * tf.reshape(random_lm[:, 0], [-1, 1])
-        flipped_lm = tf.concat(
-            values=[
-                tf.reshape(random_lm[:, 0], [-1, 1]),
-                tf.reshape(flipped_lm_xs[:, 0], [-1, 1]),
-                tf.reshape(random_lm[:, 2], [-1, 1]),
-                tf.reshape(flipped_lm_xs[:, 1], [-1, 1]),
-                tf.reshape(random_lm[:, 4], [-1, 1]),
-                tf.reshape(flipped_lm_xs[:, 2], [-1, 1]),
-                tf.reshape(random_lm[:, 6], [-1, 1]),
-                tf.reshape(flipped_lm_xs[:, 3], [-1, 1]),
-                tf.reshape(random_lm[:, 8], [-1, 1]),
-                tf.reshape(flipped_lm_xs[:, 4], [-1, 1]),
-                tf.reshape(random_lm[:, 10], [-1, 1])
-            ],
-            axis=1
-        )
-        landmarks = tf.tensor_scatter_nd_update(landmarks, random_indices, flipped_lm)
+    # Randomly flip the images horizontaly
+    random_indices = tf.where(tf.random.uniform([batch_size], 0, 2, tf.int32))
+    # Randomly flip the images
+    images = tf.tensor_scatter_nd_update(images, random_indices, tf.image.flip_left_right(tf.gather_nd(images, random_indices)))
+    # Randomly flip the bounding boxes
+    random_bb = tf.gather_nd(bboxes, random_indices)
+    flipped_bb_x = (1 - random_bb[:, 1] - random_bb[:, 3]) * random_bb[:, 0]
+    flipped_bb = tf.concat([tf.reshape(random_bb[:, 0], [-1, 1]), tf.reshape(flipped_bb_x, [-1, 1]), random_bb[:, 2:]], axis=1)
+    bboxes = tf.tensor_scatter_nd_update(bboxes, random_indices, flipped_bb)
+    # Randomly flip the landmarks
+    random_lm = tf.gather_nd(landmarks, random_indices)
+    flipped_lm_xs = (1 - random_lm[:, 1::2]) * tf.reshape(random_lm[:, 0], [-1, 1])
+    flipped_lm = tf.concat(
+        values=[
+            tf.reshape(random_lm[:, 0], [-1, 1]),
+            tf.reshape(flipped_lm_xs[:, 0], [-1, 1]),
+            tf.reshape(random_lm[:, 2], [-1, 1]),
+            tf.reshape(flipped_lm_xs[:, 1], [-1, 1]),
+            tf.reshape(random_lm[:, 4], [-1, 1]),
+            tf.reshape(flipped_lm_xs[:, 2], [-1, 1]),
+            tf.reshape(random_lm[:, 6], [-1, 1]),
+            tf.reshape(flipped_lm_xs[:, 3], [-1, 1]),
+            tf.reshape(random_lm[:, 8], [-1, 1]),
+            tf.reshape(flipped_lm_xs[:, 4], [-1, 1]),
+            tf.reshape(random_lm[:, 10], [-1, 1])
+        ],
+        axis=1
+    )
+    landmarks = tf.tensor_scatter_nd_update(landmarks, random_indices, flipped_lm)
 
-        # Randomly adjust the hue
-        random_indices = tf.where(tf.random.uniform([batch_size], 0, 2, tf.int32))
-        images = tf.tensor_scatter_nd_update(images, random_indices, tf.image.random_hue(tf.gather_nd(images, random_indices), 0.02))
+    # Randomly adjust the hue
+    random_indices = tf.where(tf.random.uniform([batch_size], 0, 2, tf.int32))
+    images = tf.tensor_scatter_nd_update(images, random_indices, tf.image.random_hue(tf.gather_nd(images, random_indices), 0.02))
 
-        # Randomly adjust the saturation
-        random_indices = tf.where(tf.random.uniform([batch_size], 0, 2, tf.int32))
-        images = tf.tensor_scatter_nd_update(images, random_indices, tf.image.random_saturation(tf.gather_nd(images, random_indices), 0.95, 1.05))
+    # Randomly adjust the saturation
+    random_indices = tf.where(tf.random.uniform([batch_size], 0, 2, tf.int32))
+    images = tf.tensor_scatter_nd_update(images, random_indices, tf.image.random_saturation(tf.gather_nd(images, random_indices), 0.95, 1.05))
 
-        # Randomly adjust the contrast
-        random_indices = tf.where(tf.random.uniform([batch_size], 0, 2, tf.int32))
-        images = tf.tensor_scatter_nd_update(images, random_indices, tf.clip_by_value(tf.image.random_contrast(tf.gather_nd(images, random_indices), 0.95, 1.05), 0, 1))
+    # Randomly adjust the contrast
+    random_indices = tf.where(tf.random.uniform([batch_size], 0, 2, tf.int32))
+    images = tf.tensor_scatter_nd_update(images, random_indices, tf.clip_by_value(tf.image.random_contrast(tf.gather_nd(images, random_indices), 0.95, 1.05), 0, 1))
 
-        # Randomly adjust the brightness
-        random_indices = tf.where(tf.random.uniform([batch_size], 0, 2, tf.int32))
-        images = tf.tensor_scatter_nd_update(images, random_indices, tf.clip_by_value(tf.image.random_brightness(tf.gather_nd(images, random_indices), 0.05), 0, 1))
+    # Randomly adjust the brightness
+    random_indices = tf.where(tf.random.uniform([batch_size], 0, 2, tf.int32))
+    images = tf.tensor_scatter_nd_update(images, random_indices, tf.clip_by_value(tf.image.random_brightness(tf.gather_nd(images, random_indices), 0.05), 0, 1))
 
-        # Zero center the images
-        images = images - mean
-
-        return (images, (labels[0], bboxes, landmarks))
-
-    return augment
+    return (images, (labels[0], bboxes, landmarks))
